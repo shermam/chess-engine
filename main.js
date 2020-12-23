da06c685277f1ed6f467cb01121e72eadcc93abb
Shermam Miranda <shermam.miranda@goodeggs.com>
2020-12-23 00:13:40 -0300

    Handle moves

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
 // @ts-check
 
-import { initialPosition } from "./board.js";
+import { initialPosition, EMPTY } from "./board.js";
 import { render } from "./renderer.js";
 
 const evaluate = (position) => position.reduce((p, c) => p + c);
 
+let position = initialPosition;
+
 const container = document.querySelector("div");
 
-render(initialPosition, container);
+render(position, container);
 
-container.addEventListener("click", (e) => {
+/** @type {number | undefined} */
+let from;
+
+container.addEventListener("mousedown", (e) => {
   if (e.target instanceof HTMLDivElement && e.target.hasAttribute("index")) {
     const index = parseInt(e.target.getAttribute("index"));
-
-    console.log(index);
+    from = index;
   }
 });
+
+container.addEventListener("mouseup", (e) => {
+  if (from == null) return;
+  if (e.target instanceof HTMLDivElement && e.target.hasAttribute("index")) {
+    const to = parseInt(e.target.getAttribute("index"));
+    if (from === to) return (from = undefined);
+
+    move({ from, to });
+
+    from = undefined;
+  }
+});
+
+/**
+ * @param param {{from: number, to: number}}
+ */
+function move({ from, to }) {
+  position[to] = position[from];
+  position[from] = EMPTY;
+  render(position, container);
+  console.log(evaluate(position)/3);
+}
 
 const evaluation = evaluate(initialPosition);
 
