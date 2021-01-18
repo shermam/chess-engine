b7814913aab13dd29c9b0972246c4845db96bc0f
Shermam Miranda <shermam.miranda@goodeggs.com>
2021-01-18 10:51:20 -0300

    Add test capabilities

---
+++ test.js
@@ -0,0 +1,92 @@
+import fs from "fs/promises";
+import { resolve } from "path";
+
+const colors = {
+  Reset: "\x1b[0m",
+  Bright: "\x1b[1m",
+  Dim: "\x1b[2m",
+  Underscore: "\x1b[4m",
+  Blink: "\x1b[5m",
+  Reverse: "\x1b[7m",
+  Hidden: "\x1b[8m",
+
+  FgBlack: "\x1b[30m",
+  FgRed: "\x1b[31m",
+  FgGreen: "\x1b[32m",
+  FgYellow: "\x1b[33m",
+  FgBlue: "\x1b[34m",
+  FgMagenta: "\x1b[35m",
+  FgCyan: "\x1b[36m",
+  FgWhite: "\x1b[37m",
+
+  BgBlack: "\x1b[40m",
+  BgRed: "\x1b[41m",
+  BgGreen: "\x1b[30m\x1b[42m",
+  BgYellow: "\x1b[43m",
+  BgBlue: "\x1b[44m",
+  BgMagenta: "\x1b[45m",
+  BgCyan: "\x1b[30m\x1b[46m",
+  BgWhite: "\x1b[47m",
+};
+
+/**
+ * @param color {keyof colors}
+ * @param text {string}
+ */
+function color(color, text) {
+  return `${colors[color]}${text}\x1b[0m`;
+}
+
+/**
+ * @param description {string}
+ * @param callback {VoidFunction}
+ */
+export function it(description, callback) {
+  try {
+    callback();
+    console.log("  ", color("BgGreen", "Pass"), description);
+  } catch (error) {
+    console.log("  ", color("BgRed", "Fail"), description);
+    console.error(error);
+    process.exitCode = 1;
+  }
+}
+
+/**
+ * @param description {string}
+ * @param callback {VoidFunction}
+ */
+export function describe(description, callback) {
+  console.log("\n");
+  console.log(color("BgCyan", description));
+  console.log();
+  callback();
+}
+
+(async () => {
+  for await (const fileName of getFiles(
+    ".",
+    new Set([".git", "node_modules"])
+  )) {
+    if (!fileName.endsWith(".test.js")) continue;
+    await import(fileName);
+  }
+})();
+
+/**
+ * @param dir {string}
+ * @param excludePath {Set<string>}
+ * @returns {AsyncGenerator<string>}
+ */
+async function* getFiles(dir, excludePath) {
+  const dirents = await fs.readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    if (excludePath.has(dirent.name)) continue;
+    const res = resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* getFiles(res, excludePath);
+    } else {
+      yield res;
+    }
+  }
+}
--- worker/linearEvaluation.test.js
+++ worker/linearEvaluation.test.js
@@ -1,57 +1,60 @@
 import assert from "assert";
 import { injectableLinearEvaluation } from "./linearEvaluation.js";
+import { it, describe } from "../test.js";
 
-(function uses_minimax_to_get_the_optimal_position_on_a_tree_test() {
-  /**
-   * Considering the tree below and considering that the root node
-   * is the maximizer. The expected result would be 6.
-   *           o
-   *         /   \
-   *       o       o
-   *      / \     / \
-   *     o   o   o   o
-   *    / \ / \ / \ / \
-   *    7 3 5 6 0 1 9 2
-   */
+describe("linearEvaluation", () => {
+  it("uses minimax to get the optimal position on a tree test", () => {
+    /**
+     * Considering the tree below and considering that the root node
+     * is the maximizer. The expected result would be 6.
+     *           o
+     *         /   \
+     *       o       o
+     *      / \     / \
+     *     o   o   o   o
+     *    / \ / \ / \ / \
+     *    7 3 5 6 0 1 9 2
+     */
 
-  const generateChildren = (() => {
-    let calls = 0;
-    return function* () {
-      switch (calls++) {
-        case 0:
-          yield new Int8Array([0]);
-          yield new Int8Array([0]);
-          break;
-        case 1:
-          yield new Int8Array([0]);
-          yield new Int8Array([0]);
-          break;
-        case 2:
-          yield new Int8Array([7]);
-          yield new Int8Array([3]);
-          break;
-        case 3:
-          yield new Int8Array([5]);
-          yield new Int8Array([6]);
-          break;
-        case 4:
-          yield new Int8Array([0]);
-          yield new Int8Array([0]);
-          break;
-        case 5:
-          yield new Int8Array([0]);
-          yield new Int8Array([1]);
-          break;
-        case 6:
-          yield new Int8Array([9]);
-          yield new Int8Array([2]);
-          break;
-      }
-    };
-  })();
+    const generateChildren = (() => {
+      let calls = 0;
+      return function* () {
+        switch (calls++) {
+          case 0:
+            yield new Int8Array([0]);
+            yield new Int8Array([0]);
+            break;
+          case 1:
+            yield new Int8Array([0]);
+            yield new Int8Array([0]);
+            break;
+          case 2:
+            yield new Int8Array([7]);
+            yield new Int8Array([3]);
+            break;
+          case 3:
+            yield new Int8Array([5]);
+            yield new Int8Array([6]);
+            break;
+          case 4:
+            yield new Int8Array([0]);
+            yield new Int8Array([0]);
+            break;
+          case 5:
+            yield new Int8Array([0]);
+            yield new Int8Array([1]);
+            break;
+          case 6:
+            yield new Int8Array([9]);
+            yield new Int8Array([2]);
+            break;
+        }
+      };
+    })();
 
-  const linearEvaluation = injectableLinearEvaluation(generateChildren);
-  const result = linearEvaluation(new Int8Array(), true, 3);
+    const linearEvaluation = injectableLinearEvaluation(generateChildren);
+    const result = linearEvaluation(new Int8Array(), true, 3);
 
-  assert.strictEqual(result, 6);
-})();
+    assert.strictEqual(result, 6);
+  });
+});
